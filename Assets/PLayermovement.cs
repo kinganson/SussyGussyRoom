f77aa6aca8736565d84a0c415248587b6a6cd703
Anson <yioanson@gmail.com>
2023-02-27 14:20:27 +0800

    sasasaa

---
+++ Assets/PLayermovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayermovement : MonoBehaviour
+{
+    public float playerSpeed;
+
+    public float JumpForce;
+    private bool IsGrounded = false;
+    public GameObject GroundCheck;
+    public float radius;
+    public LayerMask WhatIsGround;
+
+    public Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Jump();
+    }
+    public void Movement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // move player horizontally
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        transform.position += movement * playerSpeed * Time.deltaTime;
+    }
+    public void Jump()
+    {
+        IsGrounded = Physics.CheckSphere(GroundCheck.transform.position, radius, WhatIsGround);
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded)
+        {
+            rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        }
+    }
+}
--- Assets/scripts/player_movement.cs
+++ Assets/scripts/player_movement.cs
@@ -6,17 +6,39 @@ public class player_movement : MonoBehaviour
 {
    private CharacterController m_controller;
    public float m_speed = 0.7f;
-   private void Awake()
+    public float jumpForce = 10.0f; // force of player jump
+    public float gravity = -9.81f; // force of gravity
+
+    Vector3 velocity; // velocity of player movement
+    public void Awake()
    {
        m_controller = GetComponent<CharacterController>();
    }
+  
+    public void Update()
+    {
+        // check if the player is grounded
+        if (m_controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2.0f; // reset velocity to prevent character sinking into the ground
+        }
 
-   private void FixedUpdate()
-   {
-       float horizontal = Input.GetAxisRaw("Horizontal");
-       float vertical = Input.GetAxisRaw("Vertical");
-       Vector3 move_dir = (horizontal*transform.right) + (vertical * transform.forward);
-       move_dir *= m_speed;
-       m_controller.Move(move_dir * m_speed);
-   }
+        // get input for horizontal (left/right) and vertical (forward/backward) movement
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // calculate movement vector based on input and speed
+        Vector3 move = transform.right * x + transform.forward * z;
+        m_controller.Move(move * m_speed * Time.deltaTime); // move the player
+
+        // check for jump input and apply force
+        if (Input.GetButtonDown("Jump") && m_controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2.0f * gravity); // calculate jump force
+        }
+
+        // apply gravity to the player's velocity
+        velocity.y += gravity * Time.deltaTime;
+        m_controller.Move(velocity * Time.deltaTime); // move the player based on velocity
+    }
 }
